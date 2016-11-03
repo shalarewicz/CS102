5639ad75c4a3689b43a283836aca4a85f5ffedee
Stephan <shalarewicz@gmail.com>
2016-11-03 17:09:50 -0400

    All but to String and delMax are working

--- MaxPQC.java
+++ MaxPQC.java
@@ -37,29 +37,50 @@ public class MaxPQC<Key extends Comparable<Key>> implements MaxPQ<Key>{
 
  }
 
-    public void insert(Key key){
-     Node toInsert = new Node();
-     toInsert.info = key;
-     if (isEmpty()){
-       this.top = toInsert;
-       // top.left = toInsert;
-       top.up = null;
-       size++;
-     } else {
-      Node oldTop = top;
-      toInsert.left = oldTop.left;
-      toInsert.right = oldTop.right;    
-      if (greater(key, oldTop.info)) {
-       top.right = toInsert;
-       // top.left = toInsert;
-       toInsert.up = top;
-       sink(oldTop);
-       // oldTop = null;
-      }
-      else sink(toInsert);
-      size++;
-     }
-    }
+public void insert(Key key){
+  Node child = new Node();
+  child.info = key;
+  if (isEmpty()) { this.top = child;
+    // child.up = this.top; 
+    // top.right = child;
+    // top.left = child;
+    size++;
+    return;
+  }
+  Node parent = findNode(this.size / 2);
+  child.up = parent;
+  size++;
+  if (this.size % 2 == 1) {
+    parent.right = child;
+  } else {
+    parent.left = child;
+  }
+  swim(child);
+}
+
+    // public void insert(Key key){
+    //  Node toInsert = new Node();
+    //  toInsert.info = key;
+    //  if (isEmpty()){
+    //    this.top = toInsert;
+    //    // top.left = toInsert;
+    //    top.up = null;
+    //    size++;
+    //  } else {
+    //   Node oldTop = top;
+    //   toInsert.left = oldTop.left;
+    //   toInsert.right = oldTop.right;    
+    //   if (greater(key, oldTop.info)) {
+    //    top.right = toInsert;
+    //    // top.left = toInsert;
+    //    toInsert.up = top;
+    //    sink(oldTop);
+    //    // oldTop = null;
+    //   }
+    //   else sink(toInsert);
+    //   size++;
+    //  }
+    // }
 
     public boolean isEmpty(){
      return size == 0;
@@ -71,14 +92,23 @@ public class MaxPQC<Key extends Comparable<Key>> implements MaxPQ<Key>{
 
     public String toString(){
      LinkedDeque<Node> queue = new LinkedDeque<Node>();
-     Node start = top.right;
+     Node start = findNode(0);
      queue.pushLeft(start);
-     String s = "The MaxPQ from top to bottom reads";
+     String s = "The MaxPQ from top to bottom reads ";
      while (!queue.isEmpty()){
       Node next = queue.popRight();
-      s = s + next.info;
-      queue.pushLeft(next.left);
-      queue.pushLeft(next.right);
+      // try{ 
+        s = s + next.info + ", ";
+      // }
+      // catch (NullPointerException x){return s;}
+      try{
+        queue.pushLeft(next.left);
+      }
+      catch (NullPointerException x){}
+      try{
+        queue.pushLeft(next.right);
+      }
+      catch (NullPointerException x){}
      }
      return s + ".";
     }
@@ -86,6 +116,22 @@ public class MaxPQC<Key extends Comparable<Key>> implements MaxPQ<Key>{
 // Helpers
     private boolean greater(Key a, Key b){
      return a.compareTo(b) > 0;
+    }
+
+    private Node findNode(int x){
+      ResizingArrayStack<Integer> route = new ResizingArrayStack<Integer>();
+      int i = x;
+      while(i > 0){
+        route.push(i % 2);
+        i /= 2;
+      }
+      Node start = this.top;
+      while (!route.isEmpty()){
+        int next = route.pop();
+        if (next == 1) start = start.left;
+        if (next == 0) start = start.right;
+      }
+      return start;
     }
 
     private void sink(Node x){
@@ -129,12 +175,12 @@ public class MaxPQC<Key extends Comparable<Key>> implements MaxPQ<Key>{
 
      pq.insert(1);
      pq.insert(10);
-     pq.insert(3);
+     pq.insert(11);
      pq.insert(7);
      pq.insert(6);
      pq.insert(15);
      System.out.println("After inserts " + pq.toString());
-     pq.delMax();
-     System.out.println("After deledtion " + pq.toString());
+     // pq.delMax();
+     // System.out.println("After deledtion " + pq.toString());
     }
 }
