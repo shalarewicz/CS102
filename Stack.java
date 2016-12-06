1b5e4493aa175ea150da865ef021f4e87412ab8d
Stephan <shalarewicz@gmail.com>
2016-12-06 10:14:59 -0500

    Stack API update

--- Stack.java
+++ Stack.java
@@ -1,7 +1,13 @@
-public interface Stack<T> {
-    public void push(T element);
-    public T pop();
-    public T peek();
-    public boolean isEmpty();
-    public int size();
+// name: Stack.java
+// Author: Robert Muller
+// Date: Sept. 15 2016
+//
+// API for ADT Stack used in PS3
+
+public interface Stack<T>{
+	public void push(T element);
+	public T pop();
+	public T peek();
+	public boolean isEmpty();
+	public int size();
 }
